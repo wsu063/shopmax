5230ca935c35427dcd78b56d91ff04b3b0bbf668
wsu063 <wsu063@gmail.com>
2024-06-03 11:56:55 +0900

    패스워드 키 변경

--- src/main/java/com/shopmax/config/JasyptConfig.java
+++ src/main/java/com/shopmax/config/JasyptConfig.java
@@ -4,18 +4,21 @@ import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableEncryptableProperties
 public class JasyptConfig {
+    @Value("${jasypt.encryptor.password}")
+    String password;
 
     @Bean("jasyptEncryptorDES")
     public StringEncryptor stringEncryptor() {
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword(""); //암복호화에 필요한 키 지정
+        config.setPassword("password"); //암복호화에 필요한 키 지정
         config.setAlgorithm("PBEWithMD5AndDES");
         config.setKeyObtentionIterations("1000");
         config.setPoolSize("1");
