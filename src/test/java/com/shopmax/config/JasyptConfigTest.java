73724254c858ec9a6e94b004ad2506c2516e97b3
wsu063 <wsu063@gmail.com>
2024-06-03 11:33:27 +0900

    다시

--- src/test/java/com/shopmax/config/JasyptConfigTest.java
+++ src/test/java/com/shopmax/config/JasyptConfigTest.java
@@ -6,13 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class JasyptConfigTest {
-    public String key = "haseo_key"; //암복화에 필요한 키
+    public String key = ""; //암복화에 필요한 키
 
     @Test
     void stringEncryptor() {
-        String url = "jdbc:mysql://database-1.clgygis60ayl.ap-northeast-2.rds.amazonaws.com:3306/shop?serverTimezone=UTC";
-        String userName = "podoarena";
-        String password = "vhehdkfpsk";
+        String url = "";
+        String userName = "";
+        String password = "";
 
         System.out.println("En_url : " + jasyptEncoding(url));
         System.out.println("En_username : " + jasyptEncoding(userName));
